3cdb45dbdaa5d1ad4f8dcd548abd0ee13c55f155
EdgarAnt <eadv7777@gmail.com>
2024-12-06 13:08:08 -0600

    LoginFrontUpdated

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -16,6 +16,52 @@ export default function Dashboard({ auth }) {
                     </div>
                 </div>
             </div>
+
+{/* PopUp de info */}
+        
+            <div className="max-w-7xl mx-auto p-6 lg:p-8">
+
+                    <div className="mt-16">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+                            <a
+                                href="#"
+                                className="scale-100 p-6 bg-white white:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                            >
+                                <div>
+                                    <div className="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                    <img src="/assets/AxoloteCool.gif" alt="Mi Logo" />
+                                    </div>
+
+                                    <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                                        Documentation
+                                    </h2>
+
+                                    <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam quae ullam deserunt tenetur amet nulla eaque rem quidem, dignissimos commodi molestiae deleniti .
+                                    </p>
+                                </div>
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    className="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                                    />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            
+{/* Division de cuadros */}
+
+
         </AuthenticatedLayout>
     );
 }
