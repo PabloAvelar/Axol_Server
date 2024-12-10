af8b87187874448430fb27aeb72753ba1ca17cd9
EdgarAnt <eadv7777@gmail.com>
2024-12-09 20:25:14 -0600

    ExampleBranchTest

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -7,6 +7,7 @@ export default function Dashboard({ auth }) {
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
+            <p>Hola</p>
             <Head title="Dashboard" />
 
             <div className="py-12">
